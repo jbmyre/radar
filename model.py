a6f084e476ae8188801cd45a0a1e92bbe7676476
Joel Buikema <joel.buikema@cbs.com>
2018-10-18 16:10:17 -0400

    rename db
    added adverage table

--- model.py
+++ model.py
@@ -24,6 +24,20 @@ player_table = schema.Table('player', metadata,
                             schema.Column('fg_percent', types.Integer),
                             )
 
+average_player_table = schema.Table('player_average', metadata,
+                                    schema.Column('id', types.Integer, autoincrement=True, primary_key=True),
+                                    schema.Column('player_id', types.Unicode(255)),
+                                    schema.Column('team_id', types.Unicode(255),
+                                                  schema.ForeignKey('team.id'), nullable=False),
+                                    schema.Column('first_name', types.Unicode(255)),
+                                    schema.Column('last_name', types.Unicode(255)),
+                                    schema.Column('jersey_number', types.Unicode(255)),
+                                    schema.Column('points', types.Integer),
+                                    schema.Column('rebounds', types.Integer),
+                                    schema.Column('assists', types.Integer),
+                                    schema.Column('fg_percent', types.Integer),
+                                    )
+
 
 class Team(object):
     pass
@@ -33,8 +47,14 @@ class Player(object):
     pass
 
 
+class AveragePlayer(object):
+    pass
+
+
 orm.mapper(Team, team_table, properties={
-    'players':orm.relation(Player, backref='team')
+    'players': orm.relation(Player, backref='team')
 })
 
 orm.mapper(Player, player_table)
+
+orm.mapper(AveragePlayer, average_player_table)
